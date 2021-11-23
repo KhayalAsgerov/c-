65bec80e2473534940db29d7a235359e02e0d496
Xeyal Asgerov <xeyalesgerov@gmail.com>
2021-11-23 09:19:08 +0400

    x*y created

---
+++ xinyehasili/x+y/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace x_y
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("X - ededini daxil edin");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Y - ededini daxil edin");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int mult = x* y;
+            Console.WriteLine("Ededlerin hasili = " + mult);
+            Console.ReadLine();
+
+        }
+    }
+}
