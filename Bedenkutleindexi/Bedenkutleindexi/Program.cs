4951fd217156f951aaca856fe4fe1a72fe5f3529
Xeyal Asgerov <xeyalesgerov@gmail.com>
2021-11-23 09:41:32 +0400

    Bedenkutleindexi created

---
+++ Bedenkutleindexi/Bedenkutleindexi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bedenkutleindexi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Beden kutle indexinizi oyrenmek ucun cekinizi(kg) girin ");
+            int kutle = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Boyunuzu girin");
+            double boy = Convert.ToDouble(Console.ReadLine());
+            double index = kutle / (boy * boy);
+            if (index <= 18)
+            {
+                Console.WriteLine("ZEIF");
+            }
+            else if (index > 18 && index <= 25)
+            {
+                Console.WriteLine("NORMAL");
+            }
+            else 
+            {
+                Console.WriteLine("OBEZ");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
