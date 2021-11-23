1f164238d9869246c375d75ee526a6a857dd313f
Xeyal Asgerov <xeyalesgerov@gmail.com>
2021-11-23 18:14:10 +0400

    foricndefor created

---
+++ foricindefor/foricindefor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foricindefor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 11; i++)
+            { 
+            
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
