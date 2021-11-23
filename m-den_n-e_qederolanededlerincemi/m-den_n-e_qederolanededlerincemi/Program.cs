4162d3f9c3fae52201ca148ed82b5f201671ca73
Xeyal Asgerov <xeyalesgerov@gmail.com>
2021-11-23 14:12:24 +0400

    m-den_n-e_qederolanededlerincemi created

---
+++ m-den_n-e_qederolanededlerincemi/m-den_n-e_qederolanededlerincemi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m_den_n_e_qederolanededlerincemi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("M-den N-e qeder olan ededlerin cemini tapmaqucun M-ededini girin");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("N-ededini girin");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = m+1; i < n; i++)
+            {
+               sum += i;
+              
+            }
+            Console.WriteLine(m+" ile "+n+" arasindaki ededlerin cemi = " + sum);
+            
+            Console.ReadLine(); 
+
+        }
+    }
+}
