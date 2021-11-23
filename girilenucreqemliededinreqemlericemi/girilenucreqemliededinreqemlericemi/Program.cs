ca49523a4ad6f7b84d70b5f1ed820a45fa6767b6
Xeyal Asgerov <xeyalesgerov@gmail.com>
2021-11-23 17:40:15 +0400

    girilenucreqemliededinreqemlericemi created

---
+++ girilenucreqemliededinreqemlericemi/girilenucreqemliededinreqemlericemi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace girilenucreqemliededinreqemlericemi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ucreqemli eded girin");
+            int gired = Convert.ToInt32(Console.ReadLine());
+            if (gired >= 100 && gired < 1000)
+            {
+                int qaliq = gired % 100;
+               int bir1 = gired - qaliq;
+                int bir = bir1 / 100;
+                int uc = qaliq % 10;
+                int iki2 = qaliq - uc;
+                int iki = iki2 / 10;
+                Console.WriteLine("Girdiyiniz ededin reqemleri cemi = " + (bir + iki + uc));
+            }
+            else {
+                Console.WriteLine("Daxil etdiyiniz eded ucreqemli deil");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
