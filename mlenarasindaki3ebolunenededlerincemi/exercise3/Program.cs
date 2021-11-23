a576877065d8bcde3d505b6a3cf5f11ea7b7c29f
Xeyal Asgerov <xeyalesgerov@gmail.com>
2021-11-23 22:52:56 +0400

    mlenarasindaki3ebolunenededlerincemi created

---
+++ mlenarasindaki3ebolunenededlerincemi/exercise3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mlenarasindaki3ebolunenededlerincemi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, n, sum=0;
+            Console.WriteLine("m ededini girin");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("n ededini girin");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+        git:
+            if (m < n)
+            {
+                if (m % 3 == 0)
+                {
+                    sum = sum + m;
+                    m++;
+                    goto git;
+                }
+                else
+                {
+                    m++;
+                    goto git;
+                }
+            }
+            else
+            {
+                Console.WriteLine(m + " ile " + n + " arasindaki 3-e bolunen edelerin cemi =  " + sum);
+            }
+        Console.ReadLine();
+        }
+    }
+}
