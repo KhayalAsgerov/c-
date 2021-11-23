ce2ce2eb136c45b7ed33b193d01919ec1caac95d
Xeyal Asgerov <xeyalesgerov@gmail.com>
2021-11-23 14:37:20 +0400

    m_le_n_arasindakicutededlerinhasili created

---
+++ m_le_n_arasindakicutededlerinhasili/m_le_n_arasindakicutededlerinhasili/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m_le_n_arasindakicutededlerinhasili
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("M-le N-arasindaki cut ededlerin hasilin tapmaq ucun m ededini girin");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("n ededini girin");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int hasil =1;
+            for (int i=m+1; i < n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    hasil = hasil * i;
+                   
+                }
+                else
+                { 
+                }
+                
+            }
+            Console.WriteLine(m+" Ededi ile "+n+" ededi arasindaki cut ededlerin hasili = "+hasil);
+            Console.ReadLine();
+        }
+    }
+}
