5c57fb2a692252b3ef09bb9930f46a1636ee0904
Xeyal Asgerov <xeyalesgerov@gmail.com>
2021-11-24 20:10:18 +0400

    exercise 3 4 created

---
+++ exercise3/exercise3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m1, n, sum=0;
+            Console.WriteLine("M ededini daxil edin");
+            m1=Convert.ToInt32(Console.ReadLine());
+            n = Convert.ToInt32(Console.ReadLine());
+            int m = m1 + 0;
+            while ( m < n)
+            {
+                sum = sum + m;
+                
+                m++;
+
+            }
+            Console.WriteLine(m1 + " ile " + n + " arasindaki ededlerin cemi = " + (sum-m1));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ exercise4/exercise4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int uzunluk;
+              string kelime;
+              Console.WriteLine("Bir kelime giriniz");
+              kelime=Console.ReadLine();
+              uzunluk = kelime.Length;
+              for (int i = uzunluk; i > 0; i--)
+              { 
+              Console.Write(kelime.Substring(i-1,1));
+              }
+              Console.ReadLine(); */
+           
+            Console.WriteLine("bir sayi giriniz");
+            int say = Convert.ToInt32( Console.ReadLine());
+            
+
+            for (int i = 1; i < say; i++)
+            {
+                for (int x = 1; x < say; x++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine("#");
+            }
+            
+          
+            
+           Console.ReadLine();
+
+
+
+        }
+    }
+}
